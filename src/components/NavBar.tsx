3c65f1f40d997abe71aae70a7740f38d32a6513d
kevianto <kevinshahale746@gmail.com>
2024-11-09 12:24:22 +0300

    design

--- src/components/NavBar.tsx
+++ src/components/NavBar.tsx
@@ -8,7 +8,7 @@ function NavBar() {
  <div className=" p-2 flex flex-row justify-between items-center w-full md:w-1/2">
        <h2 className="text-semibold text-xl ">welcome board</h2>
        <div className="bg-green-400  p-1 rounded-sm hover:cursor-pointer hover:px-5 transition-all duration-200 ease-in-out hover:bg-red-500">
-        <FaRegStar size={32}/>
+        <FaRegStar size={34}/>
         </div>
         <div className="bg-green-400  p-3 rounded-sm hover:cursor-pointer hover:px-5 transition-all duration-200 ease-in-out hover:bg-red-500 "> Personal</div>
         <div className="bg-green-400  p-3 rounded-sm flex flex-row items-center gap-2 hover:cursor-pointer hover:px-5 transition-all duration-200 ease-in-out hover:bg-red-500"> 
